be90addfdfef4b874ca8784ce0c3b46d2b5a528a
shunt <shunt@twilio.com>
2019-10-25 15:20:43 -0700

    update conftest.py, fix indent lint

--- tests/test_event_endpoint.py
+++ tests/test_event_endpoint.py
@@ -7,39 +7,39 @@ from requests import exceptions
 
 # Method to load test case from external file
 EVENT_FILE = os.path.join(
-   os.path.dirname(__file__),
-   'events',
-   'event_endpoint.json'
+    os.path.dirname(__file__),
+    'events',
+    'event_endpoint.json'
 )
 
 @pytest.fixture()
 def event(event_file=EVENT_FILE):
-   '''Trigger event'''
-   with open(event_file) as f:
-      return {"body" : json.dumps(json.load(f))}
+    '''Trigger event'''
+    with open(event_file) as f:
+        return {"body" : json.dumps(json.load(f))}
 
 @pytest.fixture()
 def context():
-   class Context(object):
-      def __init__(self, timeout, arn_string, version_name):
-         self.function_name = "undefined"
-         self.function_version = version_name
-         self.invoked_function_arn = arn_string
-         self.memory_limit_in_mb = 0
-         self.aws_request_id = "undefined"
-         self.log_group_name = "undefined"
-         self.log_stream_name = "undefined"
-         self.identity = None
-         self.client_context = None
-         self.timeout = timeout
-         # self.duration = timedelta(seconds=timeout)
-   _context = Context(60, "arn:aws:lambda:us-west-2:123456789012:function:foobar", "2.0")
-   return _context
+    class Context(object):
+        def __init__(self, timeout, arn_string, version_name):
+            self.function_name = "undefined"
+            self.function_version = version_name
+            self.invoked_function_arn = arn_string
+            self.memory_limit_in_mb = 0
+            self.aws_request_id = "undefined"
+            self.log_group_name = "undefined"
+            self.log_stream_name = "undefined"
+            self.identity = None
+            self.client_context = None
+            self.timeout = timeout
+            # self.duration = timedelta(seconds=timeout)
+    _context = Context(60, "arn:aws:lambda:us-west-2:123456789012:function:foobar", "2.0")
+    return _context
 
 import functions.event_endpoint.lambda_function as lambda_function
 
 def test_lambda_handler(event, context):
-   #test lambda_handler
-   response = lambda_function.lambda_handler(event, context)
+    #test lambda_handler
+    response = lambda_function.lambda_handler(event, context)
 
-   assert response.get("statusCode") == 200
+    assert response.get("statusCode") == 200
--- tests/test_send_log_message.py
+++ tests/test_send_log_message.py
@@ -11,40 +11,40 @@ import functions.send_log_message.lambda_function as lambda_function
 
 @pytest.fixture()
 def message():
-   return "TEST"
+    return "TEST"
 
 @pytest.fixture()
 def sumo_ssm_name():
-   return "/socless/integration_name/message_url"
+    return "/socless/integration_name/message_url"
 
 @mock_ssm
 def test_handle_state(sumo_ssm_name, message):
-   #setup param for lambda function
-   client = boto3.client('ssm')
-   client.put_parameter(Name=sumo_ssm_name, Type='SecureString', Value='http://twilio.com', KeyId='alias/aws/ssm')
+    #setup param for lambda function
+    client = boto3.client('ssm')
+    client.put_parameter(Name=sumo_ssm_name, Type='SecureString', Value='http://twilio.com', KeyId='alias/aws/ssm')
 
-   # run lambda function
-   response = lambda_function.handle_state(sumo_ssm_name, message)
+    # run lambda function
+    response = lambda_function.handle_state(sumo_ssm_name, message)
 
-   # test response
-   assert response.get("status") == "success"
+    # test response
+    assert response.get("status") == "success"
 
 @mock_ssm
 def test_handle_state_invalid_message(sumo_ssm_name):
-   #setup param for lambda function
-   client = boto3.client('ssm')
-   client.put_parameter(Name=sumo_ssm_name, Type='SecureString', Value='http://twilio.com', KeyId='alias/aws/ssm')
+    #setup param for lambda function
+    client = boto3.client('ssm')
+    client.put_parameter(Name=sumo_ssm_name, Type='SecureString', Value='http://twilio.com', KeyId='alias/aws/ssm')
 
-   # run lambda function
-   with pytest.raises(TypeError):
-      response = lambda_function.handle_state(sumo_ssm_name, ['not', 'a', 'string'])
+    # run lambda function
+    with pytest.raises(TypeError):
+        response = lambda_function.handle_state(sumo_ssm_name, ['not', 'a', 'string'])
 
 @mock_ssm
 def test_handle_state_failed_post(sumo_ssm_name, message):
-   #setup failed POST param for lambda function
-   client = boto3.client('ssm')
-   client.put_parameter(Name=sumo_ssm_name, Type='SecureString', Value='https://httpstat.us/403', KeyId='alias/aws/ssm')
+    #setup failed POST param for lambda function
+    client = boto3.client('ssm')
+    client.put_parameter(Name=sumo_ssm_name, Type='SecureString', Value='https://httpstat.us/403', KeyId='alias/aws/ssm')
 
-   # run lambda function
-   with pytest.raises(exceptions.ConnectionError):
-      response = lambda_function.handle_state(sumo_ssm_name, message)
+    # run lambda function
+    with pytest.raises(exceptions.ConnectionError):
+        response = lambda_function.handle_state(sumo_ssm_name, message)
