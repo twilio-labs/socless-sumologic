43b84ce5c7120e7b07f5289935fba2f9420321ae
Saxon <34722463+noxasaxon@users.noreply.github.com>
2019-10-30 15:19:12 -0700

    streamline tests, autorun moto (#2)
    * streamline tests, autorun moto
    
    * update conftest.py, fix indent lint
    
    * remove unnecessary fixtures/imports
    
    * use httpstat

--- functions/send_log_message/lambda_function.py
+++ functions/send_log_message/lambda_function.py
@@ -4,20 +4,20 @@ import requests, boto3
 client = boto3.client('ssm')
 
 def handle_state(sumo_ssm_name, message):
-   if not isinstance(message, str):
-      raise TypeError("'message' argument type should be String, not {}".format(type(message)))
-   
-   response = client.get_parameter( Name=sumo_ssm_name, WithDecryption=True )
-   SUMO_URL = response['Parameter']['Value']
+    if not isinstance(message, str):
+        raise TypeError("'message' argument type should be String, not {}".format(type(message)))
+    
+    response = client.get_parameter( Name=sumo_ssm_name, WithDecryption=True )
+    SUMO_URL = response['Parameter']['Value']
 
-   sumo_response = requests.post(SUMO_URL, data = { 'message' : message } )
+    sumo_response = requests.post(SUMO_URL, data = { 'message' : message } )
 
-   #check for correct response
-   if not sumo_response.ok:
-      # failure to post response
-      raise requests.exceptions.ConnectionError('Failed to post message to SumoLogic. Status Code: {} \n Response: {}'.format(sumo_response.status_code, sumo_response.text))
+    #check for correct response
+    if not sumo_response.ok:
+        # failure to post response
+        raise requests.exceptions.ConnectionError('Failed to post message to SumoLogic. Status Code: {} \n Response: {}'.format(sumo_response.status_code, sumo_response.text))
 
-   return { "status" : "success" }
+    return { "status" : "success" }
 
 def lambda_handler(event,context):
-   return socless_bootstrap(event,context,handle_state)
+    return socless_bootstrap(event,context,handle_state)
